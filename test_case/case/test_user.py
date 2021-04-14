af82d448d077989bb5598a1608a34c65ec473ad2
xuepl <593971579@qq.com>
2021-04-14 17:42:43 +0800

    新增在前置处理器中给字典数据替换.json文件中的数据功能

--- test_case/case/test_user.py
+++ test_case/case/test_user.py
@@ -13,13 +13,12 @@ from common.send_request import SendRequest
 
 def test_register_1():
     # 发送请求前的准备动作
-    pre_process = [{"body": {"loginName": "$PHONE()$"}}
-
-                   ]
+    pre_process = [
+        {"body": {"loginName": "$PHONE()$"}}
+    ]
     # 收到响应后的后置操作
     post_process = [
         '$ASSERT_CONTAINS("resultCode": 200)$',
         "$JSON_EXTRACTOR(message,$.message,0)$"
     ]
     print(SendRequest("register.json").send_request(post_process=post_process, pre_process=pre_process).local_var)
-
